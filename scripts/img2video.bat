b967b9b2d23583fde9db5582b07246999ca3dee2
Peblo <a01275676@tec.mx>
2024-10-25 11:36:06 -0600

    feat: models folder

--- scripts/img2video.bat
+++ scripts/img2video.bat
@@ -1,1 +1,1 @@
-ffmpeg -framerate 30 -pattern_type glob -i '../tmp/*.jpg' -c:v libx264 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 30 -pattern_type glob -i "*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
