0167d6ddd68de36e84571a579d39b7c411d304ed
Peblo <a01275676@tec.mx>
2024-11-14 21:10:41 -0600

    fix: nvm start settup

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,9 @@ if ! command -v node &> /dev/null; then
     read -p "Do you want to install Node.js (y/n): " installnode
     if [[ "$installnode" == "y" ]]; then
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
         nvm install 18
         nvm use 18
     else
